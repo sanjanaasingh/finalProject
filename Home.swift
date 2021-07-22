b9f64304d6ee3b64f5b419e43f3ca8fcf0b887dd
Sarah Thomas <saraht320@gmail.com>
2021-07-22 11:36:56 -0700

    sarah's edit

--- Home.swift
+++ Home.swift
@@ -9,21 +9,21 @@ import UIKit
 
 class Home: UIViewController {
 
+    @IBOutlet weak var textFieldName: UITextField!
+    
+    @IBOutlet weak var hiNameField: UILabel!
+    
+    @IBAction func submitButton(_ sender: UIButton) {
+        
+        if let newTitle = textFieldName.text {
+            hiNameField.text = "how are you feeling today, \(newTitle)?"
+        }
+                
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
